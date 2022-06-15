47fa948dfc7d0a2b4a097b5935cc55833c7c789e
Ankit Raj <ankittrajj1996@gmail.com>
2022-06-15 18:17:46 +0530

    complete_project

--- projectop.py
+++ projectop.py
@@ -43,3 +43,41 @@ while True:
         cursor.execute(query)
         con.commit()
         print("Account Open")
+
+
+# use condition for choice 2
+# ask the user for amount to deposit
+# query to update the account.
+
+    # CASH DEPOSIT
+    elif choice == '2':
+        name = input("Enter the name")
+        amt = int(input("Enter the amt no."))
+        query = "Update x set amt = {} where name='{}".format(amt,name)
+        cursor.execute(query)
+        con.commit()
+        print('Cash Deposit')
+
+    #Cash withdrawl ----> 3
+
+#     account details-----> fetch all code
+    elif choice == '4':
+        query = "select * from x"
+        cursor.execute(query)
+        acc_detail = cursor.fetchall()
+        print(acc_detail)
+        con.commit()
+
+
+# exit use break
+    elif choice == '5':
+        break
+
+    else:
+        print("Invalid command")
+
+
+# Modify
+
+# 1.Make a relation between  withdrawl & deposit.
+# 2.Make one more choice.
